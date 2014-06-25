945ce5900b132394e7b2728a1bddbf43de0d1abb
Brady Vercher <brady@blazersix.com>
2014-06-25 09:43:40 -0700

    Add the editor style sheet to the autoprefixer task.

--- tasks/options/autoprefixer.js
+++ tasks/options/autoprefixer.js
@@ -3,6 +3,9 @@ module.exports = {
 		options: {
 			browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'android 4']
 		},
-		src: 'style.css'
+		src: [
+			'style.css',
+			'assets/css/editor-style.css'
+		]
 	}
 }
