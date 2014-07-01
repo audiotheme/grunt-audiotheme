2fbba0c9d7e6f95c2a5185c717fda0027fd2f888
Brady Vercher <brady@blazersix.com>
2014-07-01 08:08:13 -0700

    Run the pixrem task on the editor style sheet by default. Fixes #2

--- tasks/options/pixrem.js
+++ tasks/options/pixrem.js
@@ -5,6 +5,10 @@ module.exports = {
 	audiotheme: {
 		files: [
 			{
+				src: 'assets/css/editor-style.css',
+				dest: 'assets/css/editor-style.css'
+			},
+			{
 				src: 'style.css',
 				dest: 'style.css'
 			}
